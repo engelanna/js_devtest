9e6d291f6dea04d070ca6874428e4b9e5513c278
Anna Engel <anna@globalapptesting.com>
2019-10-04 22:50:39 +0200

    Use snake_case for filenames

---
+++ src/app.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import app from "./app";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<app />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
