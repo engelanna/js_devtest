847459f9a95b8573844297a2245b99040045f0bd
Anna Engel <anna@globalapptesting.com>
2019-10-05 00:23:58 +0200

    Resolve 'yarn audit' warnings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
 import "./index.css";
 import App from "./app";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./server_tools/service_worker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
