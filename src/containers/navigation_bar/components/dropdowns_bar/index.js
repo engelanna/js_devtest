f715f012900729fd8c7e60c042e2f6867a68c888
Anna Engel <anna@globalapptesting.com>
2019-10-05 22:26:34 +0200

    Fix text selection in headers

--- src/containers/navigation_bar/components/dropdowns_bar/index.js
+++ src/containers/navigation_bar/components/dropdowns_bar/index.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import VideoInformationRecordsDropdown from "./components/video_information_records_dropdown";
 
+import styles from "./dropdowns_bar.module.scss";
+
 const DropdownsBar = () => (
-  <>
+  <div className={styles.no_text_select}>
     <VideoInformationRecordsDropdown />
-  </>
+  </div>
 );
 
 export default DropdownsBar;
